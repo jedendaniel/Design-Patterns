c2df2e2546b829784148afc10f4bdee7c7af8b96
Dd <jedendaniel@gmail.com>
2018-07-24 12:42:35 +0200

    Add facade pattern

---
+++ src/com/java/design/patterns/facade/BleatMaker.java
@@ -0,0 +1,24 @@
+package com.java.design.patterns.facade;
+
+import com.java.design.patterns.sheep.BlackSheep;
+import com.java.design.patterns.sheep.SuperSheep;
+
+public class BleatMaker {
+    private BlackSheep blackSheep;
+    private SuperSheep superSheep;
+
+    public BleatMaker() {
+        blackSheep = new BlackSheep();
+        superSheep = new SuperSheep();
+    }
+
+    public void MakeBlackSheepBleat(){
+        System.out.println("This is how black sheep bleat:");
+        blackSheep.bleat();
+    }
+
+    public void MakeSuperSheepBleat(){
+        System.out.println("This is how black super bleat:");
+        superSheep.bleat();
+    }
+}
